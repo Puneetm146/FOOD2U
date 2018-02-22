bdd3c4206a183952fca70ce9996c14c62f9eb930
puneetm146 <puneetm146gmail.com>
2018-02-22 12:00:47 +0530

    puneetm

--- app/src/main/java/com/example/dell/food2u/Login.java
+++ app/src/main/java/com/example/dell/food2u/Login.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -53,13 +54,11 @@ public class Login extends AppCompatActivity {
 
         builder = new AlertDialog.Builder(Login.this);
 
-
         net_check = netCheck();
 
         if(net_check == true){
             displayNetwork();
         }
-
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
